7ee36a7e3f59232ccb34eb10411ad778e3d4f971
cx <sober_e1@qq.com>
2023-06-10 14:14:53 +0800

    init

--- lib/flutter_cc_deviceinfo_plugin.dart
+++ lib/flutter_cc_deviceinfo_plugin.dart
@@ -8,4 +8,28 @@ class FlutterCcDeviceinfoPlugin {
   Future<String?> setGzip(String data, String key) {
     return FlutterCcDeviceinfoPluginPlatform.instance.getAESUtil(data, key);
   }
+
+  Future<String?> getGeneralData() {
+    return FlutterCcDeviceinfoPluginPlatform.instance.getGeneralData();
+  }
+
+  Future<String?> getHardwareData() {
+    return FlutterCcDeviceinfoPluginPlatform.instance.getHardwareData();
+  }
+
+  Future<String?> getSimCardInfo() {
+    return FlutterCcDeviceinfoPluginPlatform.instance.getSimCardInfo();
+  }
+
+  Future<String?> getStorageData() {
+    return FlutterCcDeviceinfoPluginPlatform.instance.getStorageData();
+  }
+
+  Future<String?> getOtherData() {
+    return FlutterCcDeviceinfoPluginPlatform.instance.getOtherData();
+  }
+
+  Future<String?> getMediaFilesData() {
+    return FlutterCcDeviceinfoPluginPlatform.instance.getMediaFilesData();
+  }
 }
